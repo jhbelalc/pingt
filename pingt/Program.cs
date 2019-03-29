6fcae314ef2c171aed3841d9b24b9da92e796e22
ssdsdsd <ff>
2019-03-29 12:22:45 -0500

    Master branch

---
+++ pingt/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Timers;
+
+namespace pingt
+{
+    class Program
+    {
+        static bool _cancelled;
+        static void Main(string[] args)
+        {  
+            try
+            {
+                string cHost = "";
+                int nPing = 0, nError = 0;
+                if (args.Length>0)
+                {
+                    cHost = args[0];
+                } else
+                {
+                    cHost = "www.google.com";
+                }
+
+                Ping pPing = new Ping();
+                Console.WriteLine(System.DateTime.Now.ToString() + " Ping to " + cHost + " Press CTRL+C to exit");
+                Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
+                while (true)
+                {
+                    PingReply reply = pPing.Send(cHost, 1000);
+                    nPing++;
+                    if (reply != null)
+                    {
+                        Console.WriteLine(System.DateTime.Now.ToString() + " Reply from " + reply.Address + " bytes=32 Time: " + reply.RoundtripTime.ToString() + " Status: " + reply.Status);
+                    }
+                    else
+                    {
+                        Console.WriteLine(System.DateTime.Now.ToString() + "No response");
+                        nError++;
+                    }
+                    System.Threading.Thread.Sleep(1000);
+                    if (_cancelled)
+                    {
+                        Console.WriteLine("Total pings: " + nPing.ToString() + " Errors: " + nError.ToString());
+                        Environment.Exit(0);
+                    };
+                }
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine("ERROR in Pingt "+e.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            if (e.SpecialKey == ConsoleSpecialKey.ControlC)
+            {
+                _cancelled = true;
+                e.Cancel = true;
+            }
+        }
+    }
+}
