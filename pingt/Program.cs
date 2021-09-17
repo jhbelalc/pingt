2993f0948baaba172b8671b5d846c5962072bf6e
John Harold Belalcazar <john.belalcazar@kantaribopemedia.com>
2021-09-17 14:16:17 -0500

    Controlado error cuando se desconectaba la red durante la ejecución del pingt.

--- pingt/Program.cs
+++ pingt/Program.cs
@@ -26,25 +26,47 @@ namespace pingt
                 Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
                 while (true)
                 {
-                    PingReply reply = pPing.Send(cHost, 1000);
-                    nPing++;
-                    if (reply != null)
+                    try
                     {
-                        if (reply.Status!=IPStatus.Success){
-                            Console.ForegroundColor=ConsoleColor.Red;
+                        PingReply reply = pPing.Send(cHost, 1000);
+                        nPing++;
+                        if (reply != null)
+                        {
+                            if (reply.Status != IPStatus.Success)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                nError++;
+                            }
+                            Console.WriteLine(System.DateTime.Now.ToString() + " Reply from " + reply.Address + " bytes=32 Time: " + reply.RoundtripTime.ToString() + " Status: " + reply.Status);
+                            if (Console.ForegroundColor != ConsoleColor.White)
+                            {
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(System.DateTime.Now.ToString() + "No response");
+                            Console.ForegroundColor = ConsoleColor.White;
                             nError++;
                         }
-                        Console.WriteLine(System.DateTime.Now.ToString() + " Reply from " + reply.Address + " bytes=32 Time: " + reply.RoundtripTime.ToString() + " Status: " + reply.Status);
-                        if (Console.ForegroundColor!=ConsoleColor.White){
-                            Console.ForegroundColor=ConsoleColor.White;
-                        }
+
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        Console.ForegroundColor= ConsoleColor.Red;
-                        Console.WriteLine(System.DateTime.Now.ToString() + "No response");
-                        Console.ForegroundColor=ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error en el pingt");
+                        Console.WriteLine(ex.Message.ToString());
                         nError++;
+                        System.Threading.Thread.Sleep(1000);
+                        if (_cancelled)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Total pings: " + nPing.ToString() + " Errors: " + nError.ToString());
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Environment.Exit(0);
+                        };
+                        continue;
                     }
                     System.Threading.Thread.Sleep(1000);
                     if (_cancelled)
